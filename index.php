2b0f3087f58fce09c727537a71a5ffa7c011e66f
Sebastian Villalobos <dereklevilla45@gmail.com>
2023-06-15 19:18:26 -0600

    cambio radical

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <!-- Stylesheets & Fonts -->
+    <link href="views/assets/css/plugins.css" rel="stylesheet">
+    <link href="views/assets/css/style.css" rel="stylesheet">
+    
+</head>
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- navbar --><?php require_once "views/parts/navbar.php"; ?><!-- End: navbar -->
+        
+        <!-- Page title -->
+        <section id="page-title" data-bg-parallax="../../IMG/wallpaper2-1.png">
+            <div class="container">
+                <div class="page-title">
+                    <h1>INDEX</h1>
+                </div>
+
+            </div>
+        </section>
+        <!-- end: Page title -->
+
+
+        <!-- FOOTER --><?php require_once "views/parts/footer.php"; ?><!-- END: FOOTER -->
+    </div>
+    <!-- end: Body Inner -->
+    <!-- Scroll top -->
+    <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+    <!--Plugins-->
+    <script src="views/assest/js/jquery.js"></script>
+    <script src="views/assest/js/plugins.js"></script>
+    <script src="views/assest/js/functions.js"></script>
+</body>
+</html>
---
+++ views/parts/navbar.php
@@ -0,0 +1,43 @@
+
+<!-- INICIO: HEADER ADMIN -->
+<header id="header" data-transparent="true" data-fullwidth="true" class="dark submenu-light">
+    <div class="header-inner">
+        <div class="container">
+            <!--Logo-->
+            <div id="logo">
+                <a href="http://localhost/SC502_2C2023_G5/">
+                    <span class="logo-default">AURA BOTANICA</span>
+                    <span class="logo-dark">AURA BOTANICA</span>
+                </a>
+            </div>
+            <!--End: Logo-->
+            <!--Navigation-->
+            <div id="mainMenu">
+                <div class="container">
+                    <nav>
+                        <ul>
+                            
+                            <li class="dropdown"><a href="#">Layout</a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-submenu"><a href="#">Topbar</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="header-topbar.html">Light</a></li>
+                                    <li><a href="header-topbar-dark.html">Dark</a></li>
+                                    <li><a href="header-topbar-transparent.html">Transparent</a></li>
+                                    <li><a href="header-topbar-colored.html">Colored</a></li>
+                                    <li><a href="header-topbar-fullwidth.html">Fullwidth</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- FIN : HEADER ADMIN -->
+
+
+<!-- INICIO: HEADER USER -->
+<!-- <header></header> -->
+<!-- FIN : HEADER USER -->
