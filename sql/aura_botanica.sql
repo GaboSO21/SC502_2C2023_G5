bf8e0decbeab1a5972fa7f4bc1569321ba37810b
GaboSO21 <gso2090@gmail.com>
2023-06-24 21:38:13 -0600

    SQL issue

--- sql/aura_botanica.sql
+++ sql/aura_botanica.sql
@@ -3,23 +3,25 @@ CREATE DATABASE aura_botanica;
 USE aura_botanica;
 
 CREATE TABLE Rol (
-    idRol int not null,
+    idRol int not null AUTO_INCREMENT,
     nombre varchar(20) not null,
     PRIMARY KEY(idRol)
 );
 
 CREATE TABLE Direccion (
-    idDireccion int not null,
+    idDireccion int not null AUTO_INCREMENT,
     provincia varchar(20) not null,
     canton varchar(20) not null,
     distrito varchar(20) not null,
     codPostal int not null,
     senalesExactas varchar(500) not null,
+    idUsuario int not null,
+    FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario),
     PRIMARY KEY(idDir)
 );
 
 CREATE TABLE Usuario (
-    idUsuario int not null,
+    idUsuario int not null AUTO_INCREMENT,
     nombre varchar(45) not null,
     primApellido varchar(45) not null,
     segApellido varchar(45) not null,
@@ -27,21 +29,20 @@ CREATE TABLE Usuario (
     correo varchar(45) not null,
     contrasenna varchar(45) not null,
     idRol int not null,
-    idDir int not null,
     FOREIGN KEY(idRol) REFERENCES Rol(idRol),
-    FOREIGN KEY(idDir) REFERENCES Direccion(idDir),
     PRIMARY KEY(idUsuario)
 );
 
 CREATE TABLE TipoServicio (
-    idTipoServicio int not null,
+    idTipoServicio int not null AUTO_INCREMENT,
     nombre varchar(45) not null,
     descripcion varchar(45) not null,
     PRIMARY KEY(idTipoServicio)
 );
 
 CREATE TABLE Servicio (
-    idServicio int not null,
+    idServicio int not null AUTO_INCREMENT,
+    float int not null,
     nombre varchar(45) not null,
     img varchar(200) not null,
     descripcion varchar(45) not null,
@@ -54,7 +55,7 @@ CREATE TABLE Servicio (
 );
 
 CREATE TABLE ServicioUsuario (
-    idServicioUsuario int not null,
+    idServicioUsuario int not null AUTO_INCREMENT,
     idUsuario int not null,
     idServicio int not null,
     FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario),
@@ -63,14 +64,14 @@ CREATE TABLE ServicioUsuario (
 );
 
 CREATE TABLE TipoProducto (
-    idTipoProducto int not null,
+    idTipoProducto int not null AUTO_INCREMENT,
     nombre varchar(45) not null,
     descripcion varchar(200) not null,
     PRIMARY KEY(idTipoProducto)
 );
 
 CREATE TABLE Producto (
-    idProducto int not null,
+    idProducto int not null AUTO_INCREMENT,
     nombre varchar(45) not null,
     descripcion varchar(200) not null,
     stock int not null,
@@ -82,7 +83,7 @@ CREATE TABLE Producto (
 );
 
 CREATE TABLE Resenna (
-    idResenna int not null,
+    idResenna int not null AUTO_INCREMENT,
     estatus boolean not null,
     descripcion varchar(255) not null,
     idProducto int not null,
@@ -93,17 +94,18 @@ CREATE TABLE Resenna (
 );
 
 CREATE TABLE Compra (
-    idCompra int not null,
+    idCompra int not null AUTO_INCREMENT,
     numeroTracking varchar(200) not null,
     precioTotal float not null,
     estatus int not null, -- 0 = pendiente, 1 = en proceso, 2 = revision, 3 = cancelado, 4= entregado
+    fecha date not null,
     idUsuario int not null,
     FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario),
     PRIMARY KEY(idCompra)
 );
 
 CREATE TABLE CompraProducto (
-    idCompraProducto int not null,
+    idCompraProducto int not null AUTO_INCREMENT,
     idCompra int not null,
     idProducto int not null,
     cantidad int not null,
