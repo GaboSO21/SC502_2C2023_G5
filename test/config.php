058759cc94adc0b2de3aa244ab80f92e7d78e79a
Sebastian Villalobos <dereklevilla45@gmail.com>
2023-07-03 15:19:41 -0600

    Update config.php

--- test/config.php
+++ test/config.php
@@ -3,4 +3,5 @@ define("urlsite", "http://localhost/test");
 define("admin_users", "http://localhost/test/admin/usuarios");
 define("admin_products", "http://localhost/test/admin/productos");
 define("SITE_ROOT", "/opt/lampp/htdocs/test/");
+?>
 
